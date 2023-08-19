0dd282a9793bd7920e12f6befcfeddc466eff272
tearsful <110714634+tearsful@users.noreply.github.com>
2023-08-20 01:39:46 +0800

    Update AutoSetup.sh

--- AutoSetup.sh
+++ AutoSetup.sh
@@ -8,7 +8,7 @@ echo "下载依赖……"
 apt update
 apt install wget git unzip pip -y
 echo "克隆库……"
-git clone https://github.com/allanchen2019/mosdns-debian-install.git /opt/mosdns
+git clone https://github.com/tearsful/mosdns-debian-install.git /opt/mosdns
 chmod 777 -R /opt/mosdns
 echo "执行安装……"
 bash /opt/mosdns/install-mosdns.sh
